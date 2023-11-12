1760ea59b1fee884a9b222f564c2903b6c01bc2e
PhapLuu <phapluu2k5tqt@gmail.com>
2023-11-12 16:27:44 +0700

    update Gig model

--- api/app/models/Gig.js
+++ api/app/models/Gig.js
@@ -2,25 +2,73 @@ import mongoose, { mongo } from "mongoose";
 const Schema = mongoose.Schema;
 
 const Gig = new Schema({
-    username:{
-        type:String,
-    },
-    price:{
+    userId:{
         type: String,
+        required: true,
     },
-    img:{
-        type:String,
+    title:{
+        type: String,
+        required: true,
     },
     desc:{
-        type:Text,
+        type: Text,
+        required: true,
+    },
+    cat:{
+        type:String,
+        required: true,
+    },
+    price:{
+        type: Number,
+        required: true,
+    },
+    userAvatar:{
+        type: String,
+        required: true,
+    },
+    cover:{
+        type: String,
+        required: true,
+    },
+    images:{
+        type: [String],
+        required: false,
+    },
+    shortTitle:{
+        type: String,
+        required: true,
+    },
+    shortDesc:{
+        type: String,
+        required: true,
+    },
+    deliveryTime:{
+        type: Number,
+        required: true,
+    },
+    revisionNumber:{
+        type: Number,
+        required: true,
+    },
+    features:{
+        type: [String],
+        required: false,
+    },
+    totalStars:{
+        type: Number,
+        default: 0,
     },
     star:{
-        type:Number,
+        type: Number,
+        default: 0,
     },
-    profilePicture:{
-        type:String,
-    }
-},{
+    sales:{
+        type: Number,
+        default: 0,
+    },
+},
+{
     timestamps:true,
 })
+
 export default mongoose.model('Gig', Gig);
