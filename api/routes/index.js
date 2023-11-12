79d7296f79f3bc22dbd129776fa4fe0871d2c306
PhapLuu <phapluu2k5tqt@gmail.com>
2023-11-12 20:50:43 +0700

    update routes and controllers folder

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,6 +1,16 @@
 import userRouter from './user.js';
-
+import gigRouter from './gig.js';
+import messageRouter from './message.js';
+import orderRouter from './order.js';
+import reviewRouter from './review.js';
+import conversationRouter from './conversation.js'
 function route(app) {
-    app.use('api/user', userRouter);
+    app.use('/api/users', userRouter);
+    app.use('/api/gigs', gigRouter);
+    app.use('/api/orders', orderRouter);
+    app.use('/api/reviews',reviewRouter);
+    app.use('./api/conversations',conversationRouter);
+    app.use('./api/messages', messageRouter);
 }
+
 export default route;
