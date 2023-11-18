149f01871eca96832ccd4eee75514dcce0df1381
PhapLuu <phapluu2k5tqt@gmail.com>
2023-11-18 16:24:40 +0700

    update

---
+++ client/src/components/reviews/Reviews.jsx
@@ -0,0 +1,121 @@
+import React from "react";
+import './Reviews.scss'
+
+const Reviews = () => {
+  return (
+    <div className="reviews">
+      <h2>Reviews</h2>
+      <div className="item">
+        <div className="user">
+          <img
+            className="pp"
+            src="https://i.pinimg.com/564x/66/f2/97/66f29779c4cd961e9100d555a0a89b34.jpg"
+          />
+          <div className="info">
+            <span>John Doe</span>
+            <div className="country">
+              <i className="fa-solid fa-flag-usa"></i>
+              <span>United States</span>
+            </div>
+          </div>
+        </div>
+        <div className="stars">
+          <i className="fa-solid fa-star"></i>
+
+          <span>5</span>
+        </div>
+        <p>
+          Successful supper judge rise shirt see ranch speak leather pupil
+          weight fellow center balloon cry recently flag bottom somehow gentle
+          strong dust ask thus street given past sit alone nation silk
+          mathematics fireplace such screen arrow hope till scale deeply
+          whenever travel reader image call of laid but dog got although though
+          while single football recent ear national rush motor me natural city
+          camp yellow report broken conversation compass weather fear corn
+        </p>
+        <div className="helpful">
+          <span>Helpful?</span>
+          <i className="fa-regular fa-thumbs-up"></i>
+          <span>Yes</span>
+          <i className="fa-regular fa-thumbs-down"></i>
+          <span>No?</span>
+        </div>
+      </div>
+      <hr />
+      <div className="item">
+        <div className="user">
+          <img
+            className="pp"
+            src="https://i.pinimg.com/564x/66/f2/97/66f29779c4cd961e9100d555a0a89b34.jpg"
+          />
+          <div className="info">
+            <span>John Doe</span>
+            <div className="country">
+              <i className="fa-solid fa-flag-usa"></i>
+              <span>United States</span>
+            </div>
+          </div>
+        </div>
+        <div className="stars">
+          <i className="fa-solid fa-star"></i>
+
+          <span>5</span>
+        </div>
+        <p>
+          Successful supper judge rise shirt see ranch speak leather pupil
+          weight fellow center balloon cry recently flag bottom somehow gentle
+          strong dust ask thus street given past sit alone nation silk
+          mathematics fireplace such screen arrow hope till scale deeply
+          whenever travel reader image call of laid but dog got although though
+          while single football recent ear national rush motor me natural city
+          camp yellow report broken conversation compass weather fear corn
+        </p>
+        <div className="helpful">
+          <span>Helpful?</span>
+          <i className="fa-regular fa-thumbs-up"></i>
+          <span>Yes</span>
+          <i className="fa-regular fa-thumbs-down"></i>
+          <span>No?</span>
+        </div>
+      </div>
+      <hr />
+      <div className="item">
+        <div className="user">
+          <img
+            className="pp"
+            src="https://i.pinimg.com/564x/66/f2/97/66f29779c4cd961e9100d555a0a89b34.jpg"
+          />
+          <div className="info">
+            <span>John Doe</span>
+            <div className="country">
+              <i className="fa-solid fa-flag-usa"></i>
+              <span>United States</span>
+            </div>
+          </div>
+        </div>
+        <div className="stars">
+          <i className="fa-solid fa-star"></i>
+          <span>5</span>
+        </div>
+        <p>
+          Successful supper judge rise shirt see ranch speak leather pupil
+          weight fellow center balloon cry recently flag bottom somehow gentle
+          strong dust ask thus street given past sit alone nation silk
+          mathematics fireplace such screen arrow hope till scale deeply
+          whenever travel reader image call of laid but dog got although though
+          while single football recent ear national rush motor me natural city
+          camp yellow report broken conversation compass weather fear corn
+        </p>
+        <div className="helpful">
+          <span>Helpful?</span>
+          <i className="fa-regular fa-thumbs-up"></i>
+          <span>Yes</span>
+          <i className="fa-regular fa-thumbs-down"></i>
+          <span>No?</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Reviews;
--- client/src/pages/gig/Gig.jsx
+++ client/src/pages/gig/Gig.jsx
@@ -4,6 +4,7 @@ import Slider from "infinite-react-carousel";
 import { Link, useParams } from "react-router-dom";
 import { useQuery } from "react-query";
 import newRequest from "../../utils/newRequest";
+import Reviews from "../../components/reviews/Reviews.jsx";
 
 const Gig = () => {
   const { id } = useParams();
@@ -113,7 +114,7 @@ const Gig = () => {
                   <div className="items">
                     <div className="item">
                       <span className="title">From</span>
-                      <span className="desc">{dataUser.country || "none"}</span>
+                      <span className="desc">{dataUser.country || "Unknown"}</span>
                     </div>
                     <div className="item">
                       <span className="title">Member since</span>
@@ -139,121 +140,7 @@ const Gig = () => {
                 </div>
               </div>
             )}
-            <div className="reviews">
-              <h2>Reviews</h2>
-              <div className="item">
-                <div className="user">
-                  <img
-                    className="pp"
-                    src="https://i.pinimg.com/564x/66/f2/97/66f29779c4cd961e9100d555a0a89b34.jpg"
-                  />
-                  <div className="info">
-                    <span>John Doe</span>
-                    <div className="country">
-                      <i className="fa-solid fa-flag-usa"></i>
-                      <span>United States</span>
-                    </div>
-                  </div>
-                </div>
-                <div className="stars">
-                  <i className="fa-solid fa-star"></i>
-
-                  <span>5</span>
-                </div>
-                <p>
-                  Successful supper judge rise shirt see ranch speak leather
-                  pupil weight fellow center balloon cry recently flag bottom
-                  somehow gentle strong dust ask thus street given past sit
-                  alone nation silk mathematics fireplace such screen arrow hope
-                  till scale deeply whenever travel reader image call of laid
-                  but dog got although though while single football recent ear
-                  national rush motor me natural city camp yellow report broken
-                  conversation compass weather fear corn
-                </p>
-                <div className="helpful">
-                  <span>Helpful?</span>
-                  <i className="fa-regular fa-thumbs-up"></i>
-                  <span>Yes</span>
-                  <i className="fa-regular fa-thumbs-down"></i>
-                  <span>No?</span>
-                </div>
-              </div>
-              <hr />
-              <div className="item">
-                <div className="user">
-                  <img
-                    className="pp"
-                    src="https://i.pinimg.com/564x/66/f2/97/66f29779c4cd961e9100d555a0a89b34.jpg"
-                  />
-                  <div className="info">
-                    <span>John Doe</span>
-                    <div className="country">
-                      <i className="fa-solid fa-flag-usa"></i>
-                      <span>United States</span>
-                    </div>
-                  </div>
-                </div>
-                <div className="stars">
-                  <i className="fa-solid fa-star"></i>
-
-                  <span>5</span>
-                </div>
-                <p>
-                  Successful supper judge rise shirt see ranch speak leather
-                  pupil weight fellow center balloon cry recently flag bottom
-                  somehow gentle strong dust ask thus street given past sit
-                  alone nation silk mathematics fireplace such screen arrow hope
-                  till scale deeply whenever travel reader image call of laid
-                  but dog got although though while single football recent ear
-                  national rush motor me natural city camp yellow report broken
-                  conversation compass weather fear corn
-                </p>
-                <div className="helpful">
-                  <span>Helpful?</span>
-                  <i className="fa-regular fa-thumbs-up"></i>
-                  <span>Yes</span>
-                  <i className="fa-regular fa-thumbs-down"></i>
-                  <span>No?</span>
-                </div>
-              </div>
-              <hr />
-              <div className="item">
-                <div className="user">
-                  <img
-                    className="pp"
-                    src="https://i.pinimg.com/564x/66/f2/97/66f29779c4cd961e9100d555a0a89b34.jpg"
-                  />
-                  <div className="info">
-                    <span>John Doe</span>
-                    <div className="country">
-                      <i className="fa-solid fa-flag-usa"></i>
-                      <span>United States</span>
-                    </div>
-                  </div>
-                </div>
-                <div className="stars">
-                  <i className="fa-solid fa-star"></i>
-                  <span>5</span>
-                </div>
-                <p>
-                  Successful supper judge rise shirt see ranch speak leather
-                  pupil weight fellow center balloon cry recently flag bottom
-                  somehow gentle strong dust ask thus street given past sit
-                  alone nation silk mathematics fireplace such screen arrow hope
-                  till scale deeply whenever travel reader image call of laid
-                  but dog got although though while single football recent ear
-                  national rush motor me natural city camp yellow report broken
-                  conversation compass weather fear corn
-                </p>
-                <div className="helpful">
-                  <span>Helpful?</span>
-                  <i className="fa-regular fa-thumbs-up"></i>
-                  <span>Yes</span>
-                  <i className="fa-regular fa-thumbs-down"></i>
-                  <span>No?</span>
-                </div>
-              </div>
-            </div>
+            <Reviews></Reviews>
           </div>
           <div className="right">
             <div className="price">
