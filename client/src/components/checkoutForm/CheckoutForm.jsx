85b644946a2aaff04ec8c863ced5c45e716e741c
PhapLuu <phapluu2k5tqt@gmail.com>
2023-11-30 21:46:17 +0700

    Add Checkout component

---
+++ client/src/components/checkoutForm/CheckoutForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './CheckoutForm.scss'
+const CheckoutForm = () => {
+  return (
+    <div className='checkoutForm'>CheckoutForm</div>
+  )
+}
+
+export default CheckoutForm
