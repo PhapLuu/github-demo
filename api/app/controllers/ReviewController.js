504ed654b5a8b286cb5e4e396f025e8bc671afd2
PhapLuu <phapluu2k5tqt@gmail.com>
2023-12-02 16:21:06 +0700

    Update Review controller

--- api/app/controllers/ReviewController.js
+++ api/app/controllers/ReviewController.js
@@ -1,43 +1,55 @@
-import createError from '../../utils/createError.js';
-import Review from '../models/Review.js';
-import Gig from '../models/Gig.js';
+import createError from "../../utils/createError.js";
+import Review from "../models/Review.js";
+import Gig from "../models/Gig.js";
 
-class ReviewController{
+class ReviewController {
+  create = async (req, res, next) => {
+    // if(req.isSeller)
+    //     return next(createError('403', "Seller can't create review!"))
+    const review = new Review({
+      userId: req.userId,
+      gigId: req.body.gigId,
+      star: req.body.star,
+      desc: req.body.desc,
+    });
+    try {
+      const savedReview = await review.save();
+      await Gig.findByIdAndUpdate(req.body.gigId, {
+        $inc: { totalStars: req.body.star, starNumber: 1 },
+      });
+      res.status(201).send(savedReview);
+    } catch (error) {
+      next(error);
+    }
+  };
+  update = async (req, res, next) => {
+    const review = await Review.findById(req.params.id);
+    if (!review) return res.status(404).send("Review not found");
+    try {
+      const updatedReview = await Review.findByIdAndUpdate(
+        req.params.id,
+        { $set: req.body },
+        { new: true }
+      );
+    res.status(200).send(updatedReview)
+    } catch (error) {
+        next(error)
+    }
+  };
+  getReviews = async (req, res, next) => {
+    try {
+      const reviews = await Review.find({ gigId: req.params.gigId });
+      res.status(200).send(reviews);
+    } catch (error) {
+      next(error);
+    }
+  };
+  delete = async (req, res, next) => {
+    try {
+    } catch (error) {
+      next(error);
+    }
+  };
+}
 
-    create = async(req, res, next) => {
-        // if(req.isSeller) 
-        //     return next(createError('403', "Seller can't create review!"))
-        const review = new Review({
-            userId: req.userId,
-            gigId: req.body.gigId,
-            star: req.body.star,
-            desc: req.body.desc,
-        })
-        try {
-            const savedReview = await review.save()
-            await Gig.findByIdAndUpdate(req.body.gigId,{
-                $inc: {totalStars : req.body.star, starNumber: 1}
-            })
-            res.status(201).send(savedReview)
-        } catch (error) {
-            next(error)
-        }
-    }
-    getReviews = async(req, res, next) => {
-        try {
-            const reviews = await Review.find({gigId: req.params.gigId})
-            res.status(200).send(reviews)
-        } catch (error) {
-            next(error);
-        }
-    }
-    delete = async(req, res, next) => {
-        try {
-            
-        } catch (error) {
-            next(error)
-        }
-    }
-};
-
-export default new ReviewController;
+export default new ReviewController();
